b9d0c5787cf37b54c4f02e987259f3878528f60e
Sooham Rafiz <rafizsooham@gmail.com>
2014-12-31 17:32:15 +0800

    Added a factorisation function to math_functions library

--- math_functions.py
+++ math_functions.py
@@ -1,3 +1,5 @@
+import math
+
 fib_values = {1:1, 2:2}
 
 def fib(n):
@@ -37,3 +39,43 @@ def soe(n):
                     else:
                         primes.remove(num_2)
     return primes
+
+def factorise(n, res= None):
+    ''' (int) -> dict
+    Returns the dict of tuple of format where the first int is
+    the prime factor of the number n and the second int is the number of times
+    the prime factor occurs.
+    '''
+
+    if n <= 1:
+        return None
+
+    # we can put an upper limit to the number of numbers we need to check for
+    # factorisation with, as the quotient of the number will also be evaluated
+    # for its prime factors. This reduces the number of steps in the algorihtm
+    upper_limit = math.sqrt(n)
+    found_divisor = False
+    i = 2
+    if res == None:
+        result = {}
+    else:
+        result = res
+
+    while i <= upper_limit and not found_divisor:
+        # find the very first divisor of the number
+        # by mathematical proof the very first divisor is always prime
+        # if there are no divisors found in the while loop then the number
+        # is prime
+        quo, rem = divmod(n, i)
+        if rem == 0: # then we have found a divisor
+            # find the divisors of the quotient
+            factorise(quo, result)
+            result[i] = result.get(i, 0) + 1
+            found_divisor = True
+        i += 1
+
+    # if the value of found_divisor is still False then the number n is prime
+    if not found_divisor:
+        result[n] = result.get(n, 0) + 1
+
+    return result
