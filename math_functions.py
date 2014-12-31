3e69ba4e03ccc000371c1b8469497beecdbfa611
Sooham Rafiz <rafizsooham@gmail.com>
2014-12-31 16:49:14 +0800

    Added a sieve of Eratosthenes algorithm in math_functions library

--- math_functions.py
+++ math_functions.py
@@ -15,3 +15,25 @@ def fib(n):
         ans = fib(n-1) + fib(n-2)
         fib_values[n] = ans
         return ans
+
+def soe(n):
+    """ (int) -> list
+    Returns the list of all prime numbers upto n inclusively using the
+    Sieve of Eratosthenes algorithm.
+    """
+
+    candidates = list(range(2, n+1))
+    primes = candidates[:] # hold the final version of candidates list
+
+    for num_1 in candidates:
+        found_first_mult = False
+        if num_1 in primes:
+            for num_2 in primes:
+                # find and keep the first multiple of num_1
+                # remove all others
+                if num_2 % num_1 == 0:
+                    if not found_first_mult:
+                        found_first_mult = True
+                    else:
+                        primes.remove(num_2)
+    return primes
