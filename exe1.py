4ffaf17b7cc991a83057a67dce8a5db4f47ce125
Sooham Rafiz <rafizsooham@gmail.com>
2015-03-20 16:03:08 -0400

    Added Generator to exe1

--- exe1.py
+++ exe1.py
@@ -3,6 +3,6 @@
 
 Find the sum of all the multiples of 3 or 5 below 1000.'''
 
-multiples = [num for num in range(0,1000) if ((num % 3 == 0) or (num % 5 == 0))]
+multiples = (num for num in range(0,1000) if ((num % 3 == 0) or (num % 5 == 0)))
 print(sum(multiples))
 
